0dfbac5395496fe880ac5da069b3045ce2b6a5de
unknown <student2@CL1-04.hju>
2019-05-07 15:16:34 +0800

    update

--- www/app.js
+++ www/app.js
@@ -7,7 +7,23 @@ document.addEventListener('init', function(event) {
         .querySelector('#myNavigator')
         .pushPage('page2.html', { data: { title: 'Page 2' } });
     };
+    page.querySelector('#sayhello-button').onclick = function() {
+      ons.notification.toast('Hello there', { timeout: 2000 });
+    };
   } else if (page.id === 'page2') {
     page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+
+    page.querySelector('#page2-button').onclick = function() {
+      var content =
+        '<p style="text-align: center">Hi, this is page 2 message</p>';
+      content +=
+        '<p><ons-button id="dynamic-button">Dynamic Button</ons-button></p>';
+      page.querySelector('#page2-message').innerHTML = content;
+      page.querySelector('#dynamic-button').onclick = function() {
+        ons.notification.toast('Hello there from dynamic button', {
+          timeout: 3000
+        });
+      };
+    };
   }
 });
